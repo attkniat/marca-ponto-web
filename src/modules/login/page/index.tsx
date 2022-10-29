52d32bd6f156a471ae92902ccc53d4cc63e74e71
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-29 16:44:32 -0300

    Loading page details

--- src/modules/login/page/index.tsx
+++ src/modules/login/page/index.tsx
@@ -1,6 +1,38 @@
-import { Box, Button, Flex, Heading, Input } from "@chakra-ui/react";
+import { Box, Button, Flex, FormControl, FormHelperText, FormLabel, Heading, Input } from "@chakra-ui/react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { api } from "../../shared/lib/api";
 
 export function LoginPage() {
+
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleChange(event : ChangeEvent<HTMLInputElement>) {
+        const {name, value} = event.target
+
+        if(name === "email") {
+            setEmail(value)
+        } else if(name === "password"){
+            setPassword(value)
+        }
+    }
+
+    async function handleSubmit (event : FormEvent) {
+        event.preventDefault();
+
+        const data = {email, password};
+        setLoading(true)
+
+        try {
+            await api.post("/login", data)
+        } catch (error) {
+            
+        } finally{
+            setLoading(false)
+        }
+    }
+
     return (
         <Flex 
             as="main"
@@ -9,12 +41,33 @@ export function LoginPage() {
             direction="column"
             align="center"
             justify="center"
+            bg="gray.200"
         >
-            <Box maxWidth="600px">
+            <Box maxWidth="600px" p="24px" bg="white" borderRadius="6px">
                 <Heading size="lg" textAlign="center">Marca Ponto</Heading>
-                    <Flex direction="column" gap="16px" mt="32px">
-                        <Input maxWidth="500px" />
-                        <Button px="200px">Login</Button>
+                    <Flex direction="column" gap="16px" mt="32px" as="form" onSubmit={handleSubmit}>
+                        <FormControl>
+                            <FormLabel> 
+                                Email
+                            </FormLabel>
+                            <Input name="email" type="email" onChange={handleChange} />
+                        </FormControl>
+
+                        <FormControl>
+                            <FormLabel> 
+                                Senha
+                            </FormLabel>
+                            <Input name="password" type="password" onChange={handleChange} />
+                        </FormControl>
+
+                        <Button 
+                            px="200px"
+                            type="submit"
+                            colorScheme="blue"
+                            isLoading={loading}
+                        >
+                            Login
+                        </Button>
                     </Flex>
             </Box>
         </Flex>
