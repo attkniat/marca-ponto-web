c49cca9cd9fb192f730bb7b26f3a26c4f5171e9e
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-11-10 14:18:14 -0300

    Apresentando data de ponto marcados do usuário

--- src/modules/home/page/PontosDiaGrid.tsx
+++ src/modules/home/page/PontosDiaGrid.tsx
@@ -1,7 +1,24 @@
 import { Box, Flex, SimpleGrid, Icon, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { IoPartlySunnySharp, IoSunnySharp } from "react-icons/io5";
+import { api } from "../../shared/lib/api";
 
 export function PontosDiaGrid() {
+
+    const [dataPonto, setDataPonto] = useState(null);
+    const [dataPonto2, setDataPonto2] = useState(null);
+
+    useEffect(() => {
+
+        api.get('/get-all-pontos-by-userId-async')
+            .then( resp => {
+                setDataPonto(resp.data[0].dataCadastro);
+                setDataPonto2(resp.data[1].dataCadastro);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
 
     return <SimpleGrid gap='18px' row={2}>
         <Box py="16px" px="24px" bgColor='purple.200' borderRadius='10px' width='400px'>
@@ -9,14 +26,14 @@ export function PontosDiaGrid() {
                 <Icon as={IoSunnySharp} />
                 <Text as='b' color='purple.250' fontSize='md'> Entrada</Text>
             </Flex>
-            <Text ml="20px" mt="4px" color='purple.250' fontSize='md'>08:00</Text>
+            <Text ml="20px" mt="4px" color='purple.250' fontSize='md'>{dataPonto}</Text>
         </Box>
         <Box py="16px" px="24px" bgColor='purple.200' borderRadius='10px' width='400px'>
             <Flex align='center' gap='4px'>
                 <Icon as={IoPartlySunnySharp} />
                 <Text as='b' color='purple.250' fontSize='md'> Saída</Text>
             </Flex>
-            <Text ml="20px" mt="4px" color='purple.250' fontSize='md'>12:00</Text>
+            <Text ml="20px" mt="4px" color='purple.250' fontSize='md'>{dataPonto2}</Text>
         </Box>
     </SimpleGrid>;
 }
