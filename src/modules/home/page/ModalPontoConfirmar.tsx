c4e88e42311ee8cf5a65b73f9086b20f1cdd1952
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-11-10 22:30:14 -0300

    Listando dados do usuário logado
    Presente no modal de confirmação de ponto

--- src/modules/home/page/ModalPontoConfirmar.tsx
+++ src/modules/home/page/ModalPontoConfirmar.tsx
@@ -1,8 +1,31 @@
 import { Button, Flex, SimpleGrid, Icon, Text, ModalOverlay, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader } from "@chakra-ui/react";
-import { IoTimeSharp, IoPersonSharp, IoIdCard, IoMail } from "react-icons/io5";
+import { useEffect, useState } from "react";
+import { IoTimeSharp, IoPersonSharp, IoIdCard, IoMail, IoShieldSharp } from "react-icons/io5";
 
 export function ModalPontoConfirmar(isOpen: boolean, onClose: () => void, formatedDate: string, handleConfirm: () => Promise<void>, loading: boolean) {
     
+    const [nome, setNome] = useState<any | null>(null);
+    const [CPF, setCPF] = useState<any | null>(null);
+    const [email, setEmail] = useState<any | null>(null);
+    const [role, setRole] = useState<any | null>(null);
+
+    useEffect(() => {
+
+        const nomeC = localStorage.getItem('customerName');
+        setNome(nomeC);
+
+        const CPF = localStorage.getItem('customerCPF');
+        setCPF(CPF);
+
+        const emailC = localStorage.getItem('customerEmail');
+        setEmail(emailC);
+
+        const roleC = localStorage.getItem('customerRole');        
+        setRole(roleC);      
+
+    }, [isOpen])
+
+
     return <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay
             bg=''
@@ -17,15 +40,19 @@ export function ModalPontoConfirmar(isOpen: boolean, onClose: () => void, format
                     <Text as='b'> Dados Pessoais</Text>
                     <Flex gap='4px'>
                         <Icon as={IoPersonSharp}></Icon>
-                        <Text> Nome</Text>
+                        <Text> Nome: {nome}</Text>
                     </Flex>
                     <Flex gap='4px'>
                         <Icon as={IoIdCard}></Icon>
-                        <Text> CPF</Text>
+                        <Text> CPF: {CPF}</Text>
                     </Flex>
                     <Flex gap='4px'>
                         <Icon as={IoMail}></Icon>
-                        <Text> Email</Text>
+                        <Text> Email: {email}</Text>
+                    </Flex>
+                    <Flex gap='4px'>
+                        <Icon as={IoShieldSharp}></Icon>
+                        <Text> {role}</Text>
                     </Flex>
                     <Text as='b'> Horário</Text>
                     <Flex gap='4px'>
