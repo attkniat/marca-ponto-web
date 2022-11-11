4f4bf601a3ee654210d1f52bf9d0af3d717b902b
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-11-10 22:06:59 -0300

    Dados do Customer logado
    Criando um novo tipo para receber um obj tipado de customer.
    
    + Chamada para o endpoint que retorna o customer para alimentar os dados do modal de confimarção de ponto  a ser marcado.

--- src/modules/login/page/index.tsx
+++ src/modules/login/page/index.tsx
@@ -3,7 +3,7 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import { api } from "../../shared/lib/api";
 import { toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
-import { LoginApiResponse } from "../types";
+import { CustomerResponse, LoginApiResponse } from "../types";
 
 export function LoginPage() {
 
@@ -13,50 +13,58 @@ export function LoginPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    function handleChange(event : ChangeEvent<HTMLInputElement>) {
-        const {name, value} = event.target
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        const { name, value } = event.target
 
-        if(name === "email") {
+        if (name === "email") {
             setEmail(value)
-        } else if(name === "password") {
+        } else if (name === "password") {
             setPassword(value)
         }
     }
 
-    async function handleSubmit (event : FormEvent) {
+    async function handleSubmit(event: FormEvent) {
         event.preventDefault();
 
-        const payload = {email, password};
+        const payload = { email, password };
         setLoading(true)
 
         try {
-            const {data} = await api.post<LoginApiResponse>("/login", payload);
+            const { data } = await api.post<LoginApiResponse>("/login", payload);
             localStorage.setItem('jwtToken', data.token);
+
+            const dataCustomer = await api.get<CustomerResponse>('/User/get-customer');
+
+            localStorage.setItem('customerId', dataCustomer.data.id);
+            localStorage.setItem('customerName', dataCustomer.data.name);
+            localStorage.setItem('customerCPF', dataCustomer.data.cpf);
+            localStorage.setItem('customerEmail', dataCustomer.data.email);
+            localStorage.setItem('customerRole', dataCustomer.data.role);
 
             toast.success('Bem-Vindo!', {
                 position: "top-center",
                 autoClose: 2000,
-                });
+            });
 
-                navigate('/home');
-            
+            navigate('/home');
+
         } catch (error) {
             toast.error('Falha no Login !', {
                 position: "top-center",
                 autoClose: 2000,
-                });
+            });
 
             console.log(error);
-        } finally{
+        } finally {
             setLoading(false)
         }
     }
 
     return (
-        <Flex 
+        <Flex
             as="main"
             width="100%"
-            height="100vh" 
+            height="100vh"
             direction="column"
             align="center"
             justify="center"
@@ -64,30 +72,30 @@ export function LoginPage() {
         >
             <Box maxWidth="600px" p="24px" bg="white" borderRadius="6px">
                 <Heading size="lg" textAlign="center">Marca Ponto</Heading>
-                    <Flex direction="column" gap="16px" mt="32px" as="form" onSubmit={handleSubmit}>
-                        <FormControl>
-                            <FormLabel>
-                                Email
-                            </FormLabel>
-                            <Input name="email" type="email" onChange={handleChange} />
-                        </FormControl>
+                <Flex direction="column" gap="16px" mt="32px" as="form" onSubmit={handleSubmit}>
+                    <FormControl>
+                        <FormLabel>
+                            Email
+                        </FormLabel>
+                        <Input name="email" type="email" onChange={handleChange} />
+                    </FormControl>
 
-                        <FormControl>
-                            <FormLabel> 
-                                Senha
-                            </FormLabel>
-                            <Input name="password" type="password" onChange={handleChange} />
-                        </FormControl>
+                    <FormControl>
+                        <FormLabel>
+                            Senha
+                        </FormLabel>
+                        <Input name="password" type="password" onChange={handleChange} />
+                    </FormControl>
 
-                        <Button 
-                            px="200px"
-                            type="submit"
-                            colorScheme="blue"
-                            isLoading={loading}
-                        >
-                            Login
-                        </Button>
-                    </Flex>
+                    <Button
+                        px="200px"
+                        type="submit"
+                        colorScheme="blue"
+                        isLoading={loading}
+                    >
+                        Login
+                    </Button>
+                </Flex>
             </Box>
         </Flex>
     )
--- src/modules/login/types/index.ts
+++ src/modules/login/types/index.ts
@@ -7,3 +7,12 @@ export type LoginApiResponse = {
     token: string;
     message: string;
 };
+
+export type CustomerResponse = {
+
+    id: string;
+    name : string;
+    cpf: string;
+    email: string;
+    role: string;
+};
