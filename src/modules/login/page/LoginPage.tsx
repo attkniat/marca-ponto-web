0cb051d7f252433bb350966eefbd766c64e7f0ea
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2025-02-21 16:23:27 +0000

    import useState removed

--- src/modules/login/page/LoginPage.tsx
+++ src/modules/login/page/LoginPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useForm } from 'react-hook-form';
 import { loginschema, LoginSchema } from "../schemas";
 import { zodResolver } from '@hookform/resolvers/zod';
